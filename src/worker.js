0feda7c05a7ec03e1870d78abd78f8e45093c0be
Tony Sunny <tonysunnymails@gmail.com>
2024-08-04 23:49:09 +0530

    Add profile links

--- src/worker.js
+++ src/worker.js
@@ -1,29 +1,30 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run "npm run dev" in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run "npm run deploy" to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 const html = `<!DOCTYPE html>
 <html lang="en">
-    <head>
-        <title>Ynot Dev</title>
-    </head>
-    <body>
-        <h1>Hello World</h1>
-        <a href="https://blog.ynots.dev">Blog</a>
-    </body>
-</html>`;
+  <head>
+    <title>Ynot Dev</title>
+  </head>
+  <body>
+    <ul>
+      <li><a href="https://github.com/tony-sunny">Github</a></li>
+      <li><a href="https://blog.ynots.dev">Blog</a></li>
+      <li><a href="https://www.linkedin.com/in/ynotdev">Linkedin</a></li>
+    </ul>
+  </body>
+</html>
+<style>
+  a {
+    text-decoration: none;
+  }
+  ul {
+    list-style: none;
+  }
+</style>`;
 
 export default {
   async fetch(request, env, ctx) {
     return new Response(html, {
       headers: {
-        'content-type': 'text/html;charset=UTF-8',
+        "content-type": "text/html;charset=UTF-8",
       },
     });
   },
