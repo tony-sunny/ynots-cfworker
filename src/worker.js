c6e8aa5314bde716266bdfc7946863486ad49b43
Tony Sunny <tonysunnymails@gmail.com>
2024-08-15 23:44:32 +0530

    dark background

--- src/worker.js
+++ src/worker.js
@@ -1,21 +1,34 @@
-const html = `<!DOCTYPE html>
+const html = `<!doctype html>
 <html lang="en">
   <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>Ynot Dev</title>
   </head>
   <body>
-    <ul>
-      <li><a href="https://github.com/tony-sunny">Github</a></li>
-      <li><a href="https://blog.ynots.dev">Blog</a></li>
-      <li><a href="https://www.linkedin.com/in/ynotdev">Linkedin</a></li>
-    </ul>
+    <nav>
+      <ul class="nav-list">
+        <li>
+          <h3><a href="https://github.com/tony-sunny">Github</a></h3>
+        </li>
+        <li>
+          <h3><a href="https://blog.ynots.dev">Blog</a></h3>
+        </li>
+        <li>
+          <h3><a href="https://www.linkedin.com/in/ynotdev">Linkedin</a></h3>
+        </li>
+      </ul>
+    </nav>
   </body>
 </html>
 <style>
-  a {
-    text-decoration: none;
+  html {
+    background-color: #131313;
+    color: #424242;
   }
-  ul {
+  a {
+    color: #1326ff;
+  }
+  ul.nav-list {
     list-style: none;
   }
 </style>`;
